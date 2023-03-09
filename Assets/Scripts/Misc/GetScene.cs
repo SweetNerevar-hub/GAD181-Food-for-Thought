47cdba303725f76084de9855fc6bbe200d532db0
Cameron <1029194@student.sae.edu.au>
2023-03-09 17:15:57 +0800

    Changed script name and relocated

--- Assets/Scripts/Misc/GetScene.cs
+++ Assets/Scripts/Misc/GetScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour {
+public class GetScene : MonoBehaviour {
 
     // Is called when a Button that changes the scene is clicked
     public void ChangeScene(int sceneIndex) {
