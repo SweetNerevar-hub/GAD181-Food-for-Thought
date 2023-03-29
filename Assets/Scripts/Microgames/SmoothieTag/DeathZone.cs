398619d9e1079afe6f1d59c6d79699f8da23254a
kelecia <1028416@student.sae.edu.au>
2023-03-29 12:44:28 +0800

    endGameScript

--- Assets/Scripts/Microgames/SmoothieTag/DeathZone.cs
+++ Assets/Scripts/Microgames/SmoothieTag/DeathZone.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathZone : MonoBehaviour
-{
+{  
+    private EndGameTimer timer;
+    private void Start()
+    {
+        timer = GameObject.Find("CountDown_Text").GetComponent<EndGameTimer>();
+    }
+    
     //script kills players on fall in zone.
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
+       
+
+        if(!timer.PlayerOneWin())
+        {
+            timer.countDownDisplay.text = "Player 2";
+            Destroy(other.gameObject);
+        }
+        else
+        {
+            timer.countDownDisplay.text = "Player 1";
+            Destroy(other.gameObject);
+        }
+
+        timer.countDownTime = 0;
+        timer.StartCoroutine(timer.CountDownToEnd());
+
+
     }
+
 
 }
--- Assets/Scripts/Microgames/SmoothieTag/EndGameTimer.cs
+++ Assets/Scripts/Microgames/SmoothieTag/EndGameTimer.cs
@@ -9,13 +9,14 @@ public class EndGameTimer : MonoBehaviour
 {
     public int countDownTime;
     public TextMeshProUGUI countDownDisplay;
-
+    public GameObject playerOne;
+    public GameObject playerTwo;
 
     private void Start()
     {
         StartCoroutine(CountDownToEnd());
     }
-    IEnumerator CountDownToEnd()
+    public IEnumerator CountDownToEnd()
     {
         while (countDownTime > 0)
         {
@@ -25,15 +26,27 @@ public class EndGameTimer : MonoBehaviour
             countDownTime--;
         }
 
-        
-
-
-        Debug.Log("Game Over");
+        PlayerOneWin();
 
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(3);
 
         countDownDisplay.gameObject.SetActive(false);
+    }
+
+    public bool PlayerOneWin()
+    {
+        if (playerOne.GetComponent<ColourChange>().isTagged)
+        {
+            countDownDisplay.text = "Player 2";
+            return false;
+        }
+        else
+        {
+            countDownDisplay.text = "Player 1";
+            return true;
+        }
+
     }
 
 }
