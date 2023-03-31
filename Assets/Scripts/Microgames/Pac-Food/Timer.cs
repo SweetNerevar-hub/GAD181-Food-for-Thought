c44894a1f19c98ef0a4bc04b65c302bd672bde66
Andre <1029497@student.sae.edu.au>
2023-03-31 19:57:09 +0800

    Pac Food usability

--- Assets/Scripts/Microgames/Pac-Food/Timer.cs
+++ Assets/Scripts/Microgames/Pac-Food/Timer.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 { 
     private int gameTimer;
+    
  
     void Start()
     {
+        gameTimer = 10;
         StartCoroutine(GameTimer());
+        
     }
   
         IEnumerator GameTimer()
@@ -16,9 +20,12 @@ public class Timer : MonoBehaviour
         while (gameTimer > 0)
         {
             gameTimer--;
-
+            GetComponent<Text>().text = gameTimer.ToString();
             yield return new WaitForSeconds(1);
         }
+        yield return new WaitForSeconds(1);
+        EventManager.instance.UpdateUI(3);
+        
     }
-
+    
 }
