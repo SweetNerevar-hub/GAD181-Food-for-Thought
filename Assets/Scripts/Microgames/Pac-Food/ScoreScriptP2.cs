fbc83554ae9d271f757ddb0a84077a17ae6a32ee
Andre <1029497@student.sae.edu.au>
2023-03-31 15:24:08 +0800

    Pac Food Score

--- Assets/Scripts/Microgames/Pac-Food/ScoreScriptP2.cs
+++ Assets/Scripts/Microgames/Pac-Food/ScoreScriptP2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreScriptP1 : MonoBehaviour
+public class ScoreScriptP2 : MonoBehaviour
 {
     public Text MyscoreText;
     private int ScoreNum;
@@ -13,17 +13,19 @@ public class ScoreScriptP1 : MonoBehaviour
     void Start()
     {
         ScoreNum = 0;
-        MyscoreText.text = "Player 1 : " + ScoreNum;
+        MyscoreText.text = "Player 2 : " + ScoreNum;
     }
 
 
     private void OnTriggerEnter2D(Collider2D Point)
     {
-        if(Point.tag == "MyPoint")
+        if (Point.tag == "MyPoint")
         {
             ScoreNum += 1;
             Destroy(Point.gameObject);
-            MyscoreText = "Point" + ScoreNum;
+
         }
     }
 }
+
+
