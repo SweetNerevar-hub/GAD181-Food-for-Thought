05e974af3808977e267b9636bb6bc568003259b7
Ben Lever <1026824@student.sae.edu.au>
2023-04-05 12:56:14 +0800

    Water Hazard Characters and Audio Mostly done

---
+++ Assets/MicroGames/WaterHazard/WH_Scripts/P1Apple.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Apple : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WHMasterObject wHMasterObject = GetComponentInParent<WHMasterObject>();
+        if (wHMasterObject.p1Turn == true)
+        {
+            animator.SetBool("Player1Turn", true);
+        }
+        else
+        {
+            animator.SetBool("Player1Turn", false);
+        }
+    }
+
+
+}
---
+++ Assets/MicroGames/WaterHazard/WH_Scripts/P2Broc.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Broc : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WHMasterObject wHMasterObject = GetComponentInParent<WHMasterObject>();
+        if (wHMasterObject.p2Turn == true)
+        {
+            animator.SetBool("Player2Turn", true);
+        }
+        else
+        {
+            animator.SetBool("Player2Turn", false);
+        }
+    }
+}
