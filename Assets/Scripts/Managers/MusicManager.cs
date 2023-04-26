c98dd4d0d7178f189b62ce2a1e5b0cb01a45cf24
Ben Lever <1026824@student.sae.edu.au>
2023-04-26 14:51:50 +0800

    playtest

--- Assets/Scripts/Managers/MusicManager.cs
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -12,6 +12,8 @@ public class MusicManager : MonoBehaviour {
 
     float waitTime = 0.01f;
 
+    int currentScene;
+
     private void Awake() {
         if(instance == null) {
             instance = this;
@@ -30,6 +32,8 @@ public class MusicManager : MonoBehaviour {
     }
 
     void CallFadeAudioOut(int sceneIndex) {
+        currentScene = sceneIndex;
+
         if (musicClips[sceneIndex - 1] == null) return;
 
         StartCoroutine(FadeAudioOut(sceneIndex));
@@ -51,6 +55,8 @@ public class MusicManager : MonoBehaviour {
     }
 
     IEnumerator FadeAudioIn() {
+        if (currentScene == 2) musicClips[1] = musicClips[0];
+
         while (audioSource.volume < 0.99f) {
             audioSource.volume += waitTime;
 
